adcba1b532f015344f24b619764f182b6a4e34f5
喻林 <yulin_yl@126.com>
2020-03-11 17:05:14 +0800

    clean code for test

--- test/rumbl_web/controllers/page_controller_test.exs
+++ test/rumbl_web/controllers/page_controller_test.exs
@@ -3,6 +3,9 @@ defmodule RumblWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Rumbl"
   end
 end
+
+# The async: true flag allows the tests in this module to run concurrently with tests defined in other modules. 
+# However, tests in the same module are always executed sequentially, in random order to avoid implicit dependencies between tests.
